c6f9337184e2733aea2a27266e86c93dc5d8a552
vinanamira <mzssdaru@gmail.com>
2025-05-25 16:29:55 +0700

    Sesuaikan user_id

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\ChatbotController;
 use App\Http\Controllers\RingkasanDataController; 
 use App\Http\Controllers\UserSiteController;
 
+
 Route::middleware('auth:sanctum')->get('/user/sites', [UserSiteController::class, 'index']);
 
 // Route::middleware('auth:sanctum')->get('/dashboard', [DashboardController::class, 'index']);
@@ -24,28 +25,23 @@ Route::middleware('auth:sanctum')->post('/riwayat2', [Riwayat2Controller::class,
 // Route::post('riwayat', [RiwayatController::class, 'index']);
 // Route::post('riwayat2', [Riwayat2Controller::class, 'index']);
 
-Route::get('dashboard', [DashboardController::class, 'index']);
-Route::get('realtime', [RealtimeController::class, 'index']);
-Route::post('riwayat', [RiwayatController::class, 'index']);
-Route::post('riwayat2', [Riwayat2Controller::class, 'index']);
-
-Route::get('/area', [AreaController::class, 'index']);  
-Route::get('/area/{id}', [AreaController::class, 'show']);  
-Route::post('/area', [AreaController::class, 'store']);   
-Route::put('/area/{id}', [AreaController::class, 'update']);  
+Route::get('/area', [AreaController::class, 'index']);
+Route::get('/area/{id}', [AreaController::class, 'show']);
+Route::post('/area', [AreaController::class, 'store']);
+Route::put('/area/{id}', [AreaController::class, 'update']);
 Route::delete('/area/{id}', [AreaController::class, 'destroy']);
 
-Route::get('/sensor', [SensorController::class, 'index']);        
-Route::get('/sensor/{id}', [SensorController::class, 'show']);   
-Route::post('/sensor', [SensorController::class, 'store']);      
-Route::put('/sensor/{id}', [SensorController::class, 'update']);  
-Route::delete('/sensor/{id}', [SensorController::class, 'destroy']); 
+Route::get('/sensor', [SensorController::class, 'index']);
+Route::get('/sensor/{id}', [SensorController::class, 'show']);
+Route::post('/sensor', [SensorController::class, 'store']);
+Route::put('/sensor/{id}', [SensorController::class, 'update']);
+Route::delete('/sensor/{id}', [SensorController::class, 'destroy']);
 
-Route::get('/tanaman', [TanamanController::class, 'index']);        
-Route::get('/tanaman/{pl_id}', [TanamanController::class, 'show']);  
-Route::post('/tanaman', [TanamanController::class, 'store']);     
+Route::get('/tanaman', [TanamanController::class, 'index']);
+Route::get('/tanaman/{pl_id}', [TanamanController::class, 'show']);
+Route::post('/tanaman', [TanamanController::class, 'store']);
 Route::put('/tanaman/{pl_id}', [TanamanController::class, 'update']);
-Route::delete('/tanaman/{pl_id}', [TanamanController::class, 'destroy']); 
+Route::delete('/tanaman/{pl_id}', [TanamanController::class, 'destroy']);
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
